723967970b1661d86f0aa05bd7f7555c5aeb1357
Jean-Aime-2023 <124204227+Jean-Aime-2023@users.noreply.github.com>
2023-10-05 11:32:44 +0200

    middleware added and me route

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,7 @@
 export default function ProfilePage({ params }: any) {
   return (
     <div className="flex flex-col item-center text-center object-center justify-center min-h-screen py-2">
-      <h1>Profile</h1>
+      <h1 className="pb-8">Profile</h1>
       <hr />
       <p className="text-4xl">
         Profile page
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,9 +3,12 @@ import axios from 'axios';
 import Link from 'next/link';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState('nothing');
+
   const logout = async () => {
     try {
       axios.get('/api/users/logout');
@@ -16,17 +19,36 @@ export default function ProfilePage() {
       toast.error(error.message);
     }
   };
+
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me');
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
   return (
     <div className="flex flex-col item-center justify-center text-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2 className="p-1 rounded bg-green-400">
+        {data === 'nothing' ? (
+          'Nothing'
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
       <button
         onClick={logout}
         className="bg-orange-500 mx-[50rem] mt-4 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-indigo-500 mx-[50rem] mt-4 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded"
+      >
+        Get User deatils
       </button>
     </div>
   );
